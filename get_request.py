a52c61bd43647e9daabb0e84bf2b53d017e2b062
goranaviani <goranaviani@gmail.com>
2020-04-08 18:57:30 +0200

    example with session

--- get_request.py
+++ get_request.py
@@ -63,3 +63,21 @@ except Timeout:
     print('The request timed out')
 else:
     print('The request did not time out')
+
+
+##### Session
+import requests
+from getpass import getpass
+
+# By using a context manager, you can ensure the resources used by
+# the session will be released after use
+with requests.Session() as session:
+    session.auth = ('username', getpass())
+
+    # Instead of requests.get(), you'll use session.get()
+    response = session.get('https://api.github.com/user')
+
+# You can inspect the response just like you did before
+print(response.headers)
+print(response.json())
+
