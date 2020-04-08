f768fcdcf5cfbd251e0e4dbfbebb8f06ae351632
goranaviani <goranaviani@gmail.com>
2020-04-08 18:59:31 +0200

    transport adapters

--- get_request.py
+++ get_request.py
@@ -81,3 +81,29 @@ with requests.Session() as session:
 print(response.headers)
 print(response.json())
 
+
+
+#####
+'''
+Transport Adapters let you define a set of configurations per service you’re 
+interacting with. For example, let’s say you want all requests to 
+https://api.github.com to retry three times before finally raising 
+a ConnectionError. You would build a Transport Adapter, set its
+max_retries parameter, and mount it to an existing Session:
+'''
+import requests
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
+
+github_adapter = HTTPAdapter(max_retries=3)
+
+session = requests.Session()
+
+# Use `github_adapter` for all requests to endpoints that start with this URL
+session.mount('https://api.github.com', github_adapter)
+
+try:
+    session.get('https://api.github.com')
+except ConnectionError as ce:
+    print(ce)
+
