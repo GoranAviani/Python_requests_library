d75d375ec647e076a4e96efde84199454344115d
goranaviani <goranaviani@gmail.com>
2020-04-08 19:06:44 +0200

    First test session object

--- session_objects.py
+++ session_objects.py
@@ -1,3 +1,22 @@
 import requests
 
+#####
 
+'''
+Sessions can also be used to provide default data to the request methods. 
+This is done by providing data to the properties on a Session object:
+'''
+
+s = requests.Session()
+s.auth = ('user', 'pass')
+s.headers.update({'x-test': 'true'})
+
+# both 'x-test' and 'x-test2' are sent
+result = s.get('http://httpbin.org/headers', headers={'x-test2': 'true'})
+print(result.text)
+
+'''
+Any dictionaries that you pass to a request method will be merged with 
+the session-level values that are set. The method-level parameters override
+ session parameters.
+'''
