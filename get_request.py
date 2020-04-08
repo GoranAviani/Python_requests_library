77e97899fac461005f246c96b77ad277bebde9e5
goranaviani <goranaviani@gmail.com>
2020-04-08 18:53:52 +0200

    example with header

--- get_request.py
+++ get_request.py
@@ -35,3 +35,19 @@ json_response = response.json()
 repository = json_response['items'][0]
 print(f'Repository name: {repository["name"]}')  # Python 3.6+
 print(f'Repository description: {repository["description"]}')  # Python 3.6+
+
+
+#####
+import requests
+
+response = requests.get(
+    'https://api.github.com/search/repositories',
+    params={'q': 'requests+language:python'},
+    headers={'Accept': 'application/vnd.github.v3.text-match+json'},
+)
+
+# View the new `text-matches` array which provides information
+# about your search term within the results
+json_response = response.json()
+repository = json_response['items'][0]
+print(f'Text matches: {repository["text_matches"]}')
